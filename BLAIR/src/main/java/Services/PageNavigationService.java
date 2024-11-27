a3d661ab8c6cefab13a245a123807f8cdab1daad
Clorky <clarkisnoob434@gmail.com>
2024-11-27 18:37:35 +0800

    Added an overloaded function for passing courses

--- BLAIR/src/main/java/Services/PageNavigationService.java
+++ BLAIR/src/main/java/Services/PageNavigationService.java
@@ -1,14 +1,11 @@
 package Services;
 
-import javafx.animation.FadeTransition;
+import Controllers.InnerCoursePageController;
+import LMS.Course;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
-import java.io.IOException;
 
 public class PageNavigationService {
     public static void navigateToPage(Object sourceControl, String pageName) {
@@ -19,6 +16,24 @@ public class PageNavigationService {
 
             Scene scene = new Scene(page);
             currentStage.setScene(scene);
+            currentStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Overloaded function to pass the course selected to the InnerCoursePage
+    public static void navigateToPage(Object sourceControl, String fxmlFile, Course course) {
+        try {
+            InnerCoursePageController controller = new InnerCoursePageController();
+            controller.setCourse(course);
+
+            FXMLLoader loader = new FXMLLoader(PageNavigationService.class.getResource("/fxml/" + fxmlFile + "-page.fxml"));
+            loader.setController(controller);
+
+            Stage currentStage = (Stage)((javafx.scene.Node) sourceControl).getScene().getWindow();
+            Parent page = loader.load();
+            currentStage.setScene(new Scene(page));
             currentStage.show();
         } catch (Exception e) {
             e.printStackTrace();
