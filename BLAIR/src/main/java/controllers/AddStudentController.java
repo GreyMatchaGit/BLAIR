7b1d879fd0c6ba85b3953c0ee8cd968f85cbc97a
Clorky <clarkisnoob434@gmail.com>
2024-12-02 09:36:00 +0800

    Updated admin panel options

--- BLAIR/src/main/java/controllers/AddStudentController.java
+++ BLAIR/src/main/java/controllers/AddStudentController.java
@@ -4,17 +4,20 @@ import services.DatabaseService;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import services.PageNavigationService;
 
 import java.io.IOException;
 
 public class AddStudentController {
     @FXML
-    private Button submitBtn;
+    private Button submitBtn, backBtn;
     @FXML
     private TextField userID, firstName, middleName, lastName, userEmail, program, yearLvl;
 
     @FXML
     public void initialize() {
+        backBtn.setOnAction(event -> PageNavigationService.navigateToPage(backBtn, "admin"));
+
         submitBtn.setOnMouseClicked(event -> {
             String id = userID.getText();
             String firstName = this.firstName.getText();
