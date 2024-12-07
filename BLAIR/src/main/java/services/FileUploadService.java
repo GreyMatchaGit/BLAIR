1c0f00ee63e9dd0f2eb1f20121e07226d1bbd3d3
Clorky <clarkisnoob434@gmail.com>
2024-12-07 23:06:47 +0800

    Added more UI and functionalities to inner courses

--- BLAIR/src/main/java/services/FileUploadService.java
+++ BLAIR/src/main/java/services/FileUploadService.java
@@ -5,9 +5,12 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 
 public class FileUploadService {
-    private static final String UPLOAD_DIRECTORY = "F:\\Documents\\OOP Capstone\\BLAIR\\src\\main\\resources\\course-files";
+    private static final String UPLOAD_DIRECTORY = StringService.convertFrom(
+            Objects.requireNonNull(DatabaseService.class.getResource("/course-files/"))
+    );
 
     public void handleFileUpload(File file) {
         Path destinationPath = Path.of(UPLOAD_DIRECTORY, file.getName());
